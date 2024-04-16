a1e04a568e81695771e3ac4df4ba7e50f8d40107
nika <nika@DESKTOP-FGUKH5T>
2024-04-16 21:34:37 +0400

    Add project files.

---
+++ G19/DatabaseImporter.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace G19_ProductImport
+{
+    public class DatabaseImporter
+    {
+        private readonly string _connectionString;
+
+        public DatabaseImporter(string connectionString)
+        {
+            _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        }
+
+        public void ImportData(IEnumerable<Category> categories/*, IEnumerable<Product> products*/)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                foreach (var category in categories)
+                {
+                    foreach (var product in category.Products)
+                    {
+
+                        string NewQuery = "insert into Categories (Name, IsActive) " +
+                                      "values (@Name, @IsActive); SELECT SCOPE_IDENTITY()";
+                        string QueryChecker = "select CategoryID from Categories " +
+                                              "where Name = @Name";
+                        string insertProduct = "insert into Products (CategoryID, Code, Name, Price, IsActive, CreateDate) " +
+                                               "values (@CategoryID, @Code, @Name, @Price, @IsActive, @CreateDate)";
+
+                        using (SqlConnection conn = new SqlConnection(_connectionString))
+                        {
+                            conn.Open();
+                            SqlCommand selectCommand = new SqlCommand(QueryChecker, conn);
+                            selectCommand.Parameters.AddWithValue("@Name", category.Name);
+                            int categoryID = (int?)selectCommand.ExecuteScalar() ?? 0;
+
+                            while (categoryID == 0)
+                            {
+                                SqlCommand categoryCommand = new SqlCommand(NewQuery, conn);
+                                categoryCommand.Parameters.AddWithValue("@Name", category.Name);
+                                categoryCommand.Parameters.AddWithValue("@IsActive", category.IsActive);
+
+                                categoryID = Convert.ToInt32(categoryCommand.ExecuteScalar());
+                            }
+
+                            SqlCommand productCommand = new SqlCommand(insertProduct, conn);
+                            productCommand.Parameters.AddWithValue("@CategoryID", categoryID);
+                            productCommand.Parameters.AddWithValue("@Code", product.Code);
+                            productCommand.Parameters.AddWithValue("@Name", product.Name);
+                            productCommand.Parameters.AddWithValue("@Price", product.Price);
+                            productCommand.Parameters.AddWithValue("@IsActive", product.IsActive);
+                            productCommand.Parameters.AddWithValue("@CreateDate", DateTime.Now);
+                            productCommand.Parameters.AddWithValue("@UpdateDate", DateTime.Today);
+
+                            productCommand.ExecuteNonQuery();
+
+                            Console.WriteLine("Data inserted successfully.");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ G19/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+
+namespace G19_ProductImport
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Stopwatch stopwatch = new Stopwatch();
+            const string filePath = @"D:\Products2.txt";
+            const string connectionString = "Server=DESKTOP-FGUKH5T; Database=FirstAdonetSecondTry; Integrated Security=true; TrustServerCertificate=true";
+
+            DatabaseImporter importer = new DatabaseImporter(connectionString);
+
+
+            int i = 1;
+            try
+            {
+
+                var categories = FileManager.ReadData(filePath);
+                stopwatch.Start();
+                foreach (var category in categories)
+                {
+                    Console.WriteLine($"{i++}) {category}");
+                    foreach (var product in category.Products)
+                    {
+                        //Console.WriteLine($"\t{product}");
+                        stopwatch.Stop();
+
+                    }
+                    importer.ImportData(categories);
+                    //Console.WriteLine("Data is imported");
+                }
+                Console.WriteLine($"Time : {stopwatch.ElapsedMilliseconds}");
+
+                //importer.ImportData(categories);
+                //var importer = new DatabaseImporter(connectionString);
+                //Console.WriteLine("Data imported.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
